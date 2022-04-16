ee322c1e4d3ed367d59a426f20641fe817e8d149
ronbz <34772056+ronbz@users.noreply.github.com>
2022-04-16 15:27:58 +0300

    Update AquaPanel.java

--- src/hw2/AquaPanel.java
+++ src/hw2/AquaPanel.java
@@ -1,6 +1,17 @@
 package hw2;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class AquaPanel extends JPanel {
+public class AquaPanel extends JPanel{
+    public AquaPanel(){
+        setLayout(new GridLayout());
+        add(new JButton("Add Animal"));
+        add(new JButton("Sleep"));
+        add(new JButton("Wake Up"));
+        add(new JButton("Reset"));
+        add(new JButton("Food"));
+        add(new JButton("Info"));
+        add(new JButton("Exit"));
+    }
 }
