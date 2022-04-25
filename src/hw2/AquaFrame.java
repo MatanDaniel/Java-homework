df3c9ae50fb370cb90076872ac225ec9c651572b
ronbz <34772056+ronbz@users.noreply.github.com>
2022-04-25 18:36:05 +0300

    updated

--- src/hw2/AquaFrame.java
+++ src/hw2/AquaFrame.java
@@ -1,122 +1,95 @@
 package hw2;
 
-import javax.swing.JPanel;
-import javax.swing.JToolBar;
-import javax.swing.JButton;
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashSet;
-import java.util.*;
+import java.awt.Dimension;
 
-public class AquaPanel extends JPanel {
+public class AquaFrame extends JFrame {
 
-    private static AquaPanel single_instance = null;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Menu");
+        JLabel displayField = new JLabel();
+        frame.setVisible(true);
+        frame.setPreferredSize(new Dimension(1200, 700));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JMenuBar menu = new JMenuBar();
+        frame.setJMenuBar(menu);
+        JMenu file = new JMenu("File");
+        menu.add(file);
+        JMenuItem exit = new JMenuItem("Exit");
+        file.add(exit);
 
-    public HashSet<Swimmable> swimmers = new HashSet<Swimmable>();
-    public JPanel buttonPanel=new JPanel();
-    private AquaPanel() {
-        buttonPanel.setLayout(new GridLayout());
+        JMenuItem item1 = new JMenuItem("Image");
+        JMenuItem item2 = new JMenuItem("Blue");
+        JMenuItem item3 = new JMenuItem("None");
+        JMenu background = new JMenu("Background");
+        menu.add(background);
+        background.add(item1);
+        background.add(item2);
+        background.add(item3);
 
-        setPreferredSize(new Dimension(1200, 700));
+        JMenu help = new JMenu("Help");
+        menu.add(help);
 
-        //JToolBar toolBar = new JToolBar();
+        JMenuItem helpItem = new JMenuItem("Help");
+        help.add(helpItem);
 
-        JButton addAnimal = new JButton("Add Animal");
-        JButton sleep = new JButton("Sleep");
-        JButton wakeUp = new JButton("Wake Up");
-        JButton reset = new JButton("Reset");
-        JButton food = new JButton("Food");
-        JButton info = new JButton("Info");
-        JButton exit = new JButton("Exit");
+        helpItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, " Home Work 3 \n GUI @ Threads");
+            }
+        });
+        BorderLayout border = new BorderLayout();
+        frame.setLayout(border);
 
-        buttonPanel.add(addAnimal);
-        buttonPanel.add(sleep);
-        buttonPanel.add(wakeUp);
-        buttonPanel.add(reset);
-        buttonPanel.add(food);
-        buttonPanel.add(info);
-        buttonPanel.add(exit);
-
-        BorderLayout border=new BorderLayout();
-        this.setLayout(border);
-        this.add(buttonPanel,BorderLayout.SOUTH);
-        //add(toolBar);
+        // Singleton Design pattern
+        JPanel bottomPanel = AquaPanel.getInstance();
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+        frame.pack();
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+
+            }
+        });
+        // blue background
+        item2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getContentPane().remove(displayField);
+                frame.getContentPane().setBackground(Color.BLUE);
+                frame.revalidate();
+                frame.repaint();
             }
         });
 
-        reset.addActionListener(new ActionListener() {
+        // Returning the background to white by pressing "None" item
+        item3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                swimmers.clear();
-                repaint();
+                frame.getContentPane().remove(displayField);
+                frame.getContentPane().setBackground(Color.WHITE);
+                frame.revalidate();
+                frame.repaint();
             }
         });
 
-        sleep.addActionListener(new ActionListener() {
+        item1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (Swimmable swimmer : swimmers) {
-                    swimmer.setSuspend();
+                try {
+                    ImageIcon image1 = new ImageIcon(AquaFrame.class.getResource("image.jpg"));
+                    displayField.setIcon(image1);
+                    frame.getContentPane().add(displayField);
+                    displayField.setSize(1200, 700);
+                } catch (Exception a) {
+                    System.out.println("Could not load image!");
                 }
-            };
-        });
-
-        wakeUp.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                for (Swimmable swimmer : swimmers) {
-                    swimmer.setResume();
-                }
             }
         });
-
-        addAnimal.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new AddAnimalDialog();
-            }
-        });
-
-        // Thread thread = new Thread(new Runnable() {
-        // @Override
-        // public void run() {
-        // while (true) {
-        // try {
-        // for (Swimmable swimmer : swimmers) {
-        // swimmer.
-        // }
-        // repaint();
-        // Thread.sleep(100);
-        // } catch (InterruptedException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // }
-        // }
-        // });
-
-        // thread.start();
-    }
-
-    public static AquaPanel getInstance() {
-        if (single_instance == null)
-            single_instance = new AquaPanel();
-
-        return single_instance;
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        for (Swimmable swimmer : swimmers) {
-            swimmer.drawAnimal(g);
-        }
     }
 }
