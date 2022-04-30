66274df07e6066425c46770f1953af53ee116032
ronbz <34772056+ronbz@users.noreply.github.com>
2022-04-30 19:17:39 +0300

    Update AquaFrame.java

--- src/hw2/AquaFrame.java
+++ src/hw2/AquaFrame.java
@@ -80,11 +80,11 @@ public class AquaFrame extends JFrame {
             }
         });
     }
-    
 
-    public static void main(String[] args) 
+
+    public static void main(String[] args)
     {
         new AquaFrame();
     }
-        
+
 }
