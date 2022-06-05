94f8eda44efccfce46aed1f405d3844a8cc440b0
MatanDaniel <93178535+MatanDaniel@users.noreply.github.com>
2022-06-05 12:26:19 +0300

    Update AquaFrame.java

--- src/hw2/AquaFrame.java
+++ src/hw2/AquaFrame.java
@@ -1,35 +1,39 @@
-package hw2;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Dimension;
-import hw2.AquaPanel;
 
 public class AquaFrame extends JFrame {
+
+    AquaPanel aquarium;
+
     public AquaFrame()
     {
+        aquarium = new AquaPanel();
+        var aquariumThread = new Thread(aquarium);
         JFrame frame = new JFrame("Menu");
-        JLabel displayField = new JLabel();
         frame.setVisible(true);
         frame.setPreferredSize(new Dimension(1200, 700));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Top tool bar
         JMenuBar menu = new JMenuBar();
         frame.setJMenuBar(menu);
+
         JMenu file = new JMenu("File");
         menu.add(file);
         JMenuItem exit = new JMenuItem("Exit");
         file.add(exit);
 
-        JMenuItem item1 = new JMenuItem("Image");
-        JMenuItem item2 = new JMenuItem("Blue");
-        JMenuItem item3 = new JMenuItem("None");
+        JMenuItem image = new JMenuItem("Image");
+        JMenuItem blue = new JMenuItem("Blue");
+        JMenuItem none = new JMenuItem("None");
         JMenu background = new JMenu("Background");
         menu.add(background);
-        background.add(item1);
-        background.add(item2);
-        background.add(item3);
+
+        background.add(image);
+        background.add(blue);
+        background.add(none);
 
         JMenu help = new JMenu("Help");
         menu.add(help);
@@ -37,48 +41,25 @@ public class AquaFrame extends JFrame {
         JMenuItem helpItem = new JMenuItem("Help");
         help.add(helpItem);
 
-        helpItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, " Home Work 3 \n GUI @ Threads");
-            }
-        });
+        helpItem.addActionListener(e -> JOptionPane.showMessageDialog(null, " Home Work 3 \n GUI @ Threads"));
+
         BorderLayout border = new BorderLayout();
         frame.setLayout(border);
 
-        // Singleton Design pattern
-        JPanel bottomPanel = AquaPanel.getInstance();
-        frame.add(bottomPanel, BorderLayout.SOUTH);
+        frame.add(aquarium, BorderLayout.SOUTH);
         frame.pack();
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
 
-            }
-        });
+        exit.addActionListener(e -> System.exit(0));
         // blue background
-        item2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                AquaPanel.getInstance().changeBackground("blue");
-            }
-        });
+        blue.addActionListener(e -> aquarium.changeBackground("blue"));
 
         // Returning the background to white by pressing "None" item
-        item3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                AquaPanel.getInstance().changeBackground("white");
-            }
-        });
+        none.addActionListener(e -> aquarium.changeBackground("white"));
 
-        item1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                AquaPanel.getInstance().changeBackground("image");
-            }
-        });
+        image.addActionListener(e -> aquarium.changeBackground("image"));
+
+        aquariumThread.start();
+
     }
 
 
