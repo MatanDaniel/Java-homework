ca120e99fbceaff7daa668c2666eca119620c1c6
MatanDaniel <93178535+MatanDaniel@users.noreply.github.com>
2022-06-05 23:25:45 +0300

    Update AddAnimalDialog.java

--- src/hw2/AddAnimalDialog.java
+++ src/hw2/AddAnimalDialog.java
@@ -1,21 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
-import java.awt.GridLayout;
 
 public class AddAnimalDialog extends JDialog {
 
-    private static final String[] animals = { "Leminaria","Zostera","Fish", "Jellyfish" };
+    private static final String[] animals = { "Laminaria","Zostera","Fish", "Jellyfish" };
     private static final String[] colors = { "Black", "Red", "Blue", "Green", "Cyan", "Orange", "Yellow", "Magenta", "Pink" };
     AquaPanel aquaPanel;
     JComboBox<String> animalList = new JComboBox<>(animals);
     public JTextField sizeTextField = new JTextField(10);
     public JTextField horSpeedTextField = new JTextField(10);
     public JTextField verSpeedTextField = new JTextField(10);
+    public JTextField x_CoordinateTextField = new JTextField(10);
+    public JTextField y_CoordinateTextField = new JTextField(10);
     JComboBox<String> colorList = new JComboBox<>(colors);
     JButton submitButton = new JButton("Submit");
     JDialog dialog= new JDialog();
+
+    static private String selectedString(ItemSelectable is) {
+        Object[] selected = is.getSelectedObjects();
+        return ((selected.length == 0) ? "null" : (String)selected[0]);
+    }
 
     public AddAnimalDialog(AquaPanel panel) {
         aquaPanel = panel;
@@ -23,10 +29,10 @@ public class AddAnimalDialog extends JDialog {
     }
 
     private void createDialog() {
-        dialog.setSize(250, 300);
+        dialog.setSize(450, 300);
 
         JPanel panel = new JPanel();
-        GridLayout layout = new GridLayout(6, 2, 5, 15);
+        GridLayout layout = new GridLayout(8, 2, 5, 15);
         panel.setLayout(layout);
 
         panel.add(new JLabel("Choose Type:"));
@@ -44,6 +50,13 @@ public class AddAnimalDialog extends JDialog {
         panel.add(new JLabel("Choose Color:"));
         panel.add(colorList);
 
+        panel.add(new JLabel( "Coordinate X of Sea Creatures"));
+        panel.add(x_CoordinateTextField);
+
+        panel.add(new JLabel("Coordinate Y of Sea Creatures"));
+        panel.add(y_CoordinateTextField);
+
+
         panel.add(submitButton);
 
         // ActionListeners
@@ -52,6 +65,13 @@ public class AddAnimalDialog extends JDialog {
         dialog.add(panel);
         dialog.setVisible(true);
         dialog.setLocationRelativeTo(null);
+        animalList.addActionListener (new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+
+                ItemSelectable is = (ItemSelectable)e.getSource();
+                String name=selectedString(is);
+            }
+        });
     }
 
     public void SubmitButton(){
@@ -76,17 +96,23 @@ public class AddAnimalDialog extends JDialog {
                     JOptionPane.showMessageDialog(null, "Error: Can't add more than 5 animals!");
                     return;
                 }
-
                 AnimalFactory factory1 = new AnimalFactory(this);
                 Swimmable fac = (Swimmable) factory1.produceSeaCreature(type);
                 if(aquaPanel.isPaused)
                     fac.paused = true;
-                aquaPanel.seaCreatures.add(fac);
 
                 PlantFactory plantFac = new PlantFactory(this);
                 Immobile creature = (Immobile) plantFac.produceSeaCreature(type);
+                if (fac == null){
+                    aquaPanel.seaCreatures.add(creature);
+                }
+                else{
+                    aquaPanel.seaCreatures.add(fac);
+                }
 
-                fac.start();
+                if (fac != null)
+                    fac.start();
+
                 this.dispose();
             }
         }
@@ -106,5 +132,4 @@ public class AddAnimalDialog extends JDialog {
             default -> null;
         };
     }
-
 }
