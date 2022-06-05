84f80960883c59c184a9d6df92b282212cc8732b
MatanDaniel <93178535+MatanDaniel@users.noreply.github.com>
2022-06-05 23:24:23 +0300

    Add files via upload

---
+++ src/hw2/MarineAnimal.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public interface MarineAnimal {
+
+    public void PaintFish();
+
+    void PaintFish(SeaCreature creature, Color col);
+}
---
+++ src/hw2/MarineAnimalDecorator.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MarineAnimalDecorator implements MarineAnimal{
+    public MarineAnimal m_Animal;
+
+    @Override
+    public void PaintFish() {
+        Color color;
+
+    }
+}
