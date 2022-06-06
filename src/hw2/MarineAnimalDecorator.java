8f9e17c1463df651cc5fb11f6e285d07e37591fc
MatanDaniel <93178535+MatanDaniel@users.noreply.github.com>
2022-06-06 12:49:00 +0300

    Update MarineAnimalDecorator.java

--- src/hw2/MarineAnimalDecorator.java
+++ src/hw2/MarineAnimalDecorator.java
@@ -1,12 +1,13 @@
-import javax.swing.*;
 import java.awt.*;
 
 public class MarineAnimalDecorator implements MarineAnimal{
     public MarineAnimal m_Animal;
+    public MarineAnimalDecorator(MarineAnimal m_Animal) {
+        this.m_Animal = m_Animal;
+    }
 
     @Override
-    public void PaintFish() {
-        Color color;
-
+    public void PaintFish(Color col) {
+        ((Swimmable)m_Animal).setColor(col);
     }
 }
